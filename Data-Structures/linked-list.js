276f63056711a6db55ae57f98a66f8de3f7fe79d
FatCatCoder <christian75armand@gmail.com>
2021-12-06 14:15:40 -0500

    LL and methods to act on it FIN

--- Data-Structures/linked-list.js
+++ Data-Structures/linked-list.js
@@ -1,3 +1,17 @@
+/**
+ * Chained Ordered list of items where each node contains reference to the next node.
+ * @head get list
+ * @size list length
+ * @isEmpty boolean
+ * @indexOf returns index of given element or -1
+ * @elementAt find element @ index or undefined
+ * @add element append to the end
+ * @removeAt removes element @ index, If only one item in the list removes that node and returns the element
+ * @addAt add (index, element) does not overwrite existing nodes
+ * @remove delete given element
+ * @removeAt delete node at given index
+ */
+
 function LinkedList() {
     var length = 0;
     var head = null;
@@ -19,6 +33,7 @@ function LinkedList() {
       return this.size() === 0;
     };
 
+    /** @param findElem - element to find index of or -1 if not found */
     this.indexOf = function(findElem){
       if(this.isEmpty()) return -1 // catch out of range
 
@@ -33,6 +48,7 @@ function LinkedList() {
       return recursion(head);
     };
 
+    /** @param index - element @ index or undefined*/
     this.elementAt = function(index){
       if(index > this.size() || this.isEmpty()) {return undefined}; // catch out of range
 
@@ -47,6 +63,7 @@ function LinkedList() {
       return recursion(head);
     };
   
+    /** @param element - append to the end */
     this.add = function(element){
       length++ // increment length
       
@@ -63,6 +80,38 @@ function LinkedList() {
       // loop
       recursion(head)
     };
+
+    this.addAt = function(index, newElement){
+      if(index >= this.size() || index < 0) {return false} // catch out of range
+
+      length++ // increment length
+
+      // head replace
+      if(index === 0){ 
+        let chain = head;
+        head = new Node(newElement);
+        head.next = chain;
+        return true;
+      }
+
+      let indexPosition = 0;
+      let prevNode = head;
+  
+      // start loop
+      return recursion(head);
+      function recursion(deepNode){
+        if(indexPosition === index){ // set new node to preNode's ref, set newNode's ref to old node (ie deepNode)
+          let chain = deepNode;
+          let newChain = new Node(newElement);
+          prevNode.next = newChain;
+          newChain.next = chain;
+          return true
+        }
+        indexPosition++;
+        prevNode = deepNode;
+        return recursion(deepNode.next)
+      }
+    }
 
     this.remove = function(element){
       if(head.element === element){
@@ -89,34 +138,26 @@ function LinkedList() {
     };
 
     this.removeAt = function(index){
+      if(index >= this.size() || index < 0) {return null} // catch out of range
+      if(this.size() === 1) {let r = head.element; head = null; length--; return r} // if only one element, delete and return it
+
       let indexPosition = 0;
       let prevNode = head;
-  
+
+      // keep memory of prevNode, recursively dig into chain using ref of the next node
+      return recursion(head);
       function recursion(deepElement){
         if(indexPosition === index){
-          length-- 
-          return prevNode.next = deepElement.next;
+          length--;
+          prevNode.next = deepElement.next; // replace upper node with the removed node's chain.
+          return deepElement.element;
         }
-        if(deepElement.next !== null){
-          prevNode = deepElement;
-          recursion(deepElement.next)
+        if(deepElement.next !== null){ 
+          indexPosition++;
+          prevNode = deepElement; // save prev node, continue loop
+          return recursion(deepElement.next)
         }
-        return 'Removed'
       }
-
-      // loop
-      recursion(head)
     };
 
   }
-
-const LL = new LinkedList();
-LL.add('a');
-LL.add('b');
-LL.add('c');
-LL.add('d');
-
-console.log(LL.indexOf('a'))
-console.log(LL.indexOf('b'))
-console.log(LL.indexOf('c'))
- 
