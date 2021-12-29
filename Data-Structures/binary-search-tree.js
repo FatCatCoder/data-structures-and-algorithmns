ff73550206570a7f6ed7b4c2b47dc8ea4f15f868
FatCatCoder <christian75armand@gmail.com>
2021-12-29 14:51:56 -0500

    BST find min max branches WIP

--- Data-Structures/binary-search-tree.js
+++ Data-Structures/binary-search-tree.js
@@ -88,35 +88,100 @@ class BinarySearchTree {
 
         let path = this.root;
         while(path.value != null){
-            if(path.value === input){return true;}
+            if(path.value === input) return true;
             
             while(input > path.value){
-                if(path.right === null){return false} // not found
+                if(path.right === null) return false; // not found
                 if(path.right === input){
-                    return true
+                    return true;
                 }
                 else path = path.right;
             }
             while(input < path.value){
-                if(path.left === null){return false} // not found
+                if(path.left === null) return false; // not found
                 if(path.left === input){
-                    return true
+                    return true;
                 }
                 else path = path.left;
             }
         }
     }
+
+    /** 
+     * returns lowest branch || -1
+     * @type int
+     */
+    findMinHeight(){
+        if(this.root === null) return -1; // null case
+        
+        let min = 1;
+
+        // looping until every branch node reaches double null leaves, 
+        // sets the min based on less than operator and current level
+        loop(this.root, 1) 
+        function loop(deepNode, level){
+           if(deepNode.left === null || deepNode.right === null){
+            console.log('check   ', deepNode.value, min, level);
+                if(level < min){ min = level }; // if min is already set, do nothing
+           }
+           else{
+               console.log(deepNode.value, min, level);
+               min++;
+               if(deepNode.left !== null) {loop(deepNode.left, ++level)}
+               if(deepNode.right !== null) {loop(deepNode.right, ++level)}
+           }
+        };
+        console.log('MIN: ', min);
+        return min;
+    }
+
+    /** 
+     * returns highest branch || -1
+     * @type int
+     */
+     findMaxHeight(){
+        if(this.root === null) return -1; // null case
+        
+        let max = 1;
+
+        // looping until every branch node reaches double null leaves, 
+        // sets the max based on less than operator and current level
+        loop(this.root, 1) 
+        function loop(deepNode, level){
+           if(deepNode.left === null || deepNode.right === null){
+                return max > level ? null : max = level; // if max is already set, do nothing
+           }
+           else{
+               loop(deepNode.left, ++level)
+               loop(deepNode.right, ++level)
+           }
+        };
+        console.log('MAX: ', max);
+        return max;
+    }
 }
 
 module.exports = BinarySearchTree
 
-const tree = new BinarySearchTree();
-tree.add(10);
-tree.add(15);
-tree.add(5); 
-tree.add(7);
-tree.add(2);
-tree.add(17);
-// tree.add(12);
+// const tree = new BinarySearchTree();
+// const tree2 = new BinarySearchTree();
 
-log(tree.findMinHeight())
+// const treeValues = [15, 10, 20, 5, 12, 17, 25, 2, 8, 14, 13, 12.5];
+// treeValues.forEach(x => tree.add(x));
+
+// const tree2Values = [15, 14, 13, 12, 11, 10, 9, 8, 7];
+// tree2Values.forEach(x => tree2.add(x));
+
+// log(tree);
+//tree.findMinHeight();
+// console.log(tree2);
+// tree2.findMinHeight();
+//tree.findMaxHeight();
+
+// tree.add(10);
+// tree.add(15);
+// tree.add(5); 
+// tree.add(7);
+// tree.add(2);
+// tree.add(17);
+// tree.add(12);
--- tests/ds/binary-search-tree.test.ts
+++ tests/ds/binary-search-tree.test.ts
@@ -12,51 +12,87 @@ describe('Binary Search Tree', () =>{
 
     */
 
-    it('New BST()', () => {
-        const tree = new BST();
-        expect(tree).not.toBeNull();
-    });
+    // it('New BST()', () => {
+    //     const tree = new BST();
+    //     expect(tree).not.toBeNull();
+    // });
 
-    it('add()', () => {
-        const tree = new BST();
+    // it('add()', () => {
+    //     const tree = new BST();
 
-        tree.add(10); // root
-        expect(tree.root.value).toEqual(10);
+    //     tree.add(10); // root
+    //     expect(tree.root.value).toEqual(10);
 
-        tree.add(15); // Right
-        expect(tree.root.right.value).toEqual(15);
+    //     tree.add(15); // Right
+    //     expect(tree.root.right.value).toEqual(15);
 
-        tree.add(5); // Left
-        expect(tree.root.left.value).toEqual(5);
+    //     tree.add(5); // Left
+    //     expect(tree.root.left.value).toEqual(5);
 
-        tree.add(7); // LR
-        expect(tree.root.left.right.value).toEqual(7);
+    //     tree.add(7); // LR
+    //     expect(tree.root.left.right.value).toEqual(7);
 
-        tree.add(2); // LL
-        expect(tree.root.left.left.value).toEqual(2);
+    //     tree.add(2); // LL
+    //     expect(tree.root.left.left.value).toEqual(2);
 
-        tree.add(17); // RR
-        expect(tree.root.right.right.value).toEqual(17);
+    //     tree.add(17); // RR
+    //     expect(tree.root.right.right.value).toEqual(17);
 
-        tree.add(12); // RL
-        expect(tree.root.right.left.value).toEqual(12);
+    //     tree.add(12); // RL
+    //     expect(tree.root.right.left.value).toEqual(12);
 
 
 
-        // console.debug(null, tree.root.left.right.value)
+    //     // console.debug(null, tree.root.left.right.value)
         
+    // });
+
+    // it('Find', () => {
+    //     const tree = new BST();
+    //     tree.add(10);
+    //     tree.add(15);
+    //     tree.add(5); 
+    //     tree.add(7);
+    //     tree.add(2);
+    //     tree.add(17);
+    //     tree.add(12);
+    //     expect(tree).not.toBeNull();
+    // });
+
+    it('FindMinHeight()', () => {
+         // Arrange
+         const tree = new BST();
+         const tree2 = new BST();
+ 
+         // Act
+         const treeValues = [15, 10, 20, 5, 12, 17, 25, 2, 8, 14, 13, 12.5];
+         treeValues.forEach(x => tree.add(x));
+ 
+         const tree2Values = [15, 14, 13, 12, 11, 10, 9, 8, 7, 20, 17, 25];
+         tree2Values.forEach(x => tree2.add(x));
+ 
+         // Assert
+         expect(tree2.findMinHeight()).toEqual(3);
+         expect(tree.findMinHeight()).toEqual(3);
+         
     });
 
-    it('Find', () => {
-        const tree = new BST();
-        tree.add(10);
-        tree.add(15);
-        tree.add(5); 
-        tree.add(7);
-        tree.add(2);
-        tree.add(17);
-        tree.add(12);
-        expect(tree).not.toBeNull();
-    });
+    // it('FindMaxHeight()', () => {
+    //     // Arrange
+    //     const tree = new BST();
+    //     const tree2 = new BST();
+
+    //     // Act
+    //     const treeValues = [15, 10, 20, 5, 12, 17, 25, 2, 8, 14, 13, 12.5];
+    //     treeValues.forEach(x => tree.add(x));
+
+    //     const tree2Values = [15, 14, 13, 12, 11, 10, 9, 8, 7, 20, 17];
+    //     tree2Values.forEach(x => tree2.add(x));
+
+    //     // Assert
+    //     expect(tree2.findMaxHeight()).toEqual(9);
+    //     expect(tree.findMaxHeight()).toEqual(6);
+        
+    // });
     
 })
