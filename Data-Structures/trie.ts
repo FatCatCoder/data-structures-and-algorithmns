dbf9bfe4c6938eb93c734289d39801ecb05ac638
FatCatCoder <christian75armand@gmail.com>
2022-06-03 19:33:24 -0400

    trie remove

--- Data-Structures/trie.ts
+++ Data-Structures/trie.ts
@@ -32,12 +32,16 @@ class Trie {
 
             if(idx == newStr.length-1){
                 NextPath.setEnd()
-                path.keys.set(elm, NextPath);
-            }   
+                if(!path.keys.has(elm))
+                     path.keys.set(elm, NextPath);
+                else path.setEnd()
+            }
+
             else if (!path.keys.has(elm)) {
                 path.keys.set(elm, NextPath);
                 path = NextPath;
             }
+            
             else{
                 path = path.keys.get(elm);
             } 
@@ -48,32 +52,38 @@ class Trie {
       if(!this.hasWord(delWord)) return false
 
       let root = this.root;
+      let roots: TrieNode[] = [];
 
       delWord.split("").forEach((letter, index) => {
+        roots.push(root);
           if (index == delWord.length-1 && root.keys.get(letter).isEnd()) {
             if(root.keys.get(letter)?.keys?.size == 0){ //root.keys.size <= 1 && 
                 root.keys.delete(letter);
             }
-            else root.keys.get(letter).unSetEnd();
+            else {
+              root.keys.get(letter).unSetEnd();
+            }
           }
 
-          // // displays letters and next letter of keys
-          // let allkeys : any= [];
-          // for(let i of root.keys.keys()){
-          //   let tmp : any= [];
-          //   for(let z of root.keys.get(i).keys.keys()){
-              
-          //     tmp.push(z)
-          //   }
-          //   allkeys.push({letter: i, next: tmp})
-          // }
-
-          // console.log(letter, 
-          //   JSON.stringify({keys: allkeys, end: root.isEnd()}), 
-          //   root.keys.get(letter).keys.keys().next().value);
           root = root.keys.get(letter); 
       });
 
+      // Clean up
+      const CleanUp = () => {
+        root = this.root;
+      
+        delWord.split("").forEach((letter, index) => {
+          if(root?.keys != undefined && root?.keys?.get(letter)?.keys != undefined)
+          {
+            if(root.keys.size == 1 && root.keys.get(letter).keys.size == 0 && !root.keys.get(letter).isEnd()){
+              root.keys.delete(letter)
+            }
+            root = root.keys.get(letter); 
+          }
+        });
+      }
+
+      CleanUp()
       return true
     }
 
@@ -96,7 +106,7 @@ class Trie {
     
         function Loop(root: any, word: any) {
           if (root.keys.size != 0) {
-            root.keys.forEach((value: any,key: any, map: any) => {
+            root.keys.forEach((value: any, key: any, map: any) => {
               Loop(value, word.concat(key));
            })
             if (root.isEnd()) {
@@ -113,59 +123,21 @@ class Trie {
 
         return words;
       };
-
-      // Recursive function to delete a key from given Trie
- remove(root: any,key: any ,depth: any)
-{
-    // If tree is empty
-        if (root == null)
-            return null;
-  
-        // If last character of key is being processed
-        if (depth == key.length) {
-  
-            // This node is no more end of word after
-            // removal of given key
-            if (root.isEnd())
-                root.unSetEnd();
-  
-            // If given is not prefix of any other word
-            if (root.keys.size == 0) {
-                root = null;
-            }
-  
-            return root;
-        }
-  
-        // If not last character, recur for the child
-        // obtained using ASCII value
-        let index = key[depth].charCodeAt(0) - 'a'.charCodeAt(0);
-        root.children[index] =
-            this.remove(root.children[index], key, depth + 1);
-  
-        // If root does not have any child (its only child got
-        // deleted), and it is not end of another word.
-        if (root.keys.size==0 && root.isEnd() == false){
-            root = null;
-        }
-  
-        return root;
-}
 };
 
 const tree = new Trie;
       tree.add("he");
       tree.add("help");
       tree.add("hello");
-      // tree.add("hells");
-      tree.add("all");
+      tree.add("hellos");
       tree.add("al");
+      tree.add("all");
+      // tree.add("al");
       tree.add("zoe");
       tree.add("zoo");
 
 log(tree.print());
 
-// log(tree.delete("hello"))
-tree.remove(tree.root, "hello", "hello".length)
+tree.delete("al")
 
 log(tree.print());
