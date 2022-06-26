7415bc903094fec40cd818349772385c40c773e9
FatCatCoder <christian75armand@gmail.com>
2022-06-26 17:29:39 -0400

    ++hash table, ++linkedlist.js to .ts

--- Data-Structures/hash-table.ts
+++ Data-Structures/hash-table.ts
@@ -1,3 +1,5 @@
+import { LinkedList, LinkedListNode } from "./linked-list";
+
 const _log = require('../utils/prettyprint') 
 
 /** 
@@ -28,7 +30,6 @@ class HashTable<T>{
         return hashStringify(preHash); // hash
     }
 
-    // Todo: add collision handling  with linkedlist
     insert(data: T): void{
         let hashed = this.hashCode(data);
         this.table[hashed] = data;
@@ -46,6 +47,54 @@ class HashTable<T>{
     
 }
 
+/** 
+    Uses Linked List for collision avoidance but will causes dupes to be saved! 
+*/
+class HashTableWithCollisions<T>{
+    public table: {[k: number]: LinkedList<T>};
+
+    constructor(){
+        this.table = {};
+    }
+
+    // A basic hash F^n, just for POC
+    hashCode(toHash: T): number {
+        let preHash = JSON.stringify(toHash); // unsafe!!
+
+        function hashStringify(hashable: string){
+            var hash = 0;
+            for (var i = 0; i < hashable.length; i++) {
+                var code = hashable.charCodeAt(i);
+                hash = ((hash<<5)-hash)+code;
+                hash = hash & hash; // Convert to 32bit integer
+            }
+            return hash;
+        }
+        return hashStringify(preHash); // hash
+    }
+
+    insert(data: T): void{
+        let hashed = this.hashCode(data);
+
+        if(this.exists(data))// add new LL Node
+             this.table[hashed].insert(data);
+        else this.table[hashed] = new LinkedList<T>(data);  // Create LL with data
+    }
+
+    delete(data: T){
+        let hashed = this.hashCode(data);
+        if(this.exists(data)){
+            this.table[hashed].delete(data);
+        }
+        else delete this.table[hashed];
+    }
+
+    exists = (data: T) =>
+        this.table[this.hashCode(data)] != undefined;
+
+    getTable = () => this.table;  
+}
+
 const ht = new HashTable<any>();
 
 ht.insert({"key": "hello"});
@@ -53,4 +102,13 @@ ht.insert({"key": "world"});
 
 _log(ht.table)
 
+
+
+const htc = new HashTableWithCollisions<any>();
+
+htc.insert({"key": "hello"});
+htc.insert({"key": "hello"});
+htc.insert({"key": "world"});
+
+console.log(htc.table[htc.hashCode({"key": "hello"})].size()) // 2
 
--- Data-Structures/linked-list.ts
+++ Data-Structures/linked-list.ts
@@ -1,3 +1,12 @@
+class LinkedListNode<T> {
+    public value: T;
+    public next : LinkedListNode<T> | null | undefined;
+
+  constructor(value: T, next: LinkedListNode<T> | null | undefined = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
 
 /**
  * Chained Ordered list of items where each node contains reference to the next node.
@@ -13,6 +22,157 @@
  * @removeAt delete node at given index
  */
 
+class LinkedList<T> {
+  private _head: LinkedListNode<T> | null | undefined;
+  private _length: number;
+
+  constructor(initData: T | null = null){
+    this._head = initData != null? new LinkedListNode<T>(initData): null;
+    this._length = initData != null? 1: 0;
+  }
+
+  head = () => this._head;
+  size = () => this._length;
+  isEmpty = () => this.size() === 0;
+
+  /** @param element - append to the end */
+  insert = (element: T) => {
+    const node = new LinkedListNode<T>(element);
+    if (this.head()) {
+      let current = this._head;
+      while (current?.next !== null) {
+        current = current?.next;
+      }
+      current.next = node;
+    }
+    else {
+      this._head = node;
+    }
+    this._length++;
+  };
+
+  insertAt(index: number, newElement: T){
+    if(index >= this.size() || index < 0) {return false} // catch out of range
+
+    this._length++ // increment length
+
+    // head replace
+    if(index === 0){ 
+      let chain = this._head;
+      this._head = new LinkedListNode<T>(newElement);
+      this._head.next = chain;
+      return true;
+    }
+
+    let indexPosition = 0;
+    let prevNode = this._head;
+
+    // loop
+    const recursion = (deepNode: LinkedListNode<T> | null | undefined): boolean => {
+      if(indexPosition === index){ // set new node to preNode's ref, set newNode's ref to old node (ie deepNode)
+        let chain = deepNode;
+        let newChain = new LinkedListNode<T>(newElement); // swap
+        prevNode!.next = newChain;
+        newChain.next = chain;
+        return true
+      }
+
+      indexPosition++;
+      prevNode = deepNode;
+      return recursion(deepNode?.next)
+    }
+
+    return recursion(this._head);
+  }
+
+  /** @param findElem - element to find index of or -1 if not found */
+  indexOf(findElem: T){
+    if(this.isEmpty()) return -1 // catch out of range
+
+    let index = 0;
+    const recursion = (deepElement: LinkedListNode<T> | null | undefined): number => {
+      if(deepElement?.value === findElem) return index;
+      else if(deepElement?.value === null) return -1;
+      index++;
+      return recursion(deepElement?.next);
+    }
+    // loop
+    return recursion(this._head);
+  };
+
+  /** @param index - element @ index or undefined*/
+  elementAt(index: number){
+    if(index > this.size() || this.isEmpty()) {return undefined}; // catch out of range
+
+    let counter = 0;
+    const recursion = (deepElement: LinkedListNode<T> | null | undefined): T | undefined => {
+      if(index === counter) return deepElement?.value;
+      else if(deepElement?.next === null) return undefined;
+      counter++;
+      return recursion(deepElement?.next);
+    }
+    // loop
+    return recursion(this._head);
+  };
+
+  delete(element: T){
+    if(this._head?.value === element){
+      this._length--;
+      this._head = this._head.next;
+      return true;
+    }
+
+    let prevNode = this._head;
+
+    const recursion = (deepElement: LinkedListNode<T> | null | undefined) => {
+      if(deepElement?.value === element){
+        this._length-- 
+        return prevNode!.next = deepElement.next;
+      }
+      if(deepElement?.next !== null){
+        prevNode = deepElement;
+        recursion(deepElement?.next)
+      }
+      return true;
+    }
+
+    // loop
+    return recursion(this._head?.next)
+  };
+
+  deleteAt(index: number){
+    if(index >= this.size() || index < 0) {return null} // catch out of range
+    if(this.size() === 1) {let r = this._head?.value; this._head = null; this._length--; return r} // if only one element, delete and return it
+
+    let indexPosition = 0;
+    let prevNode = this._head;
+
+    // keep memory of prevNode, recursively dig into chain using ref of the next node      
+    const recursion = (deepElement: LinkedListNode<T> | null | undefined): T | LinkedListNode<T> | null | undefined => {
+      if(indexPosition === index){
+        this._length--;
+        prevNode!.next = deepElement?.next; // replace upper node with the removed node's chain.
+        return deepElement!.value;
+      }
+      if(deepElement?.next !== null){ 
+        indexPosition++;
+        prevNode = deepElement; // save prev node, continue loop
+        return recursion(deepElement?.next)
+      }
+    }
+    return recursion(this._head);
+  };
+}
+
+export { LinkedList, LinkedListNode }
+
+
+
+
+
+  /** OLD js function class  version*/
+
+  
 // function LinkedList<T>() {
 //     var length: number;
 //     var head: LLNode<T>
@@ -174,161 +334,3 @@
 //   }
 
 //   export { LinkedList }
-
-
-
-
-
-
-
-
-  class LinkedListNode<T> {
-      public value: T;
-      public next : LinkedListNode<T> | null | undefined;
-
-    constructor(value: T, next: LinkedListNode<T>? = null) {
-      this.value = value;
-      this.next = next;
-    }
-  }
-
-  class LinkedList<T> {
-    private _head: LinkedListNode<T> | null | undefined;
-    private _length: number;
-
-    constructor(){
-      this._head = null;
-      this._length = 0;
-    }
-  
-    head = () => this._head;
-    size = () => this._length;
-    isEmpty = () => this.size() === 0;
-
-    /** @param element - append to the end */
-    insert = (element: T) => {
-      const node = new LinkedListNode<T>(element);
-      if (this.head()) {
-        let current = this._head;
-        while (current?.next !== null) {
-          current = current?.next;
-        }
-        current.next = node;
-      }
-      else {
-        this._head = node;
-      }
-      length++;
-    };
-
-    /** @param findElem - element to find index of or -1 if not found */
-    this.indexOf = function(findElem){
-      if(this.isEmpty()) return -1 // catch out of range
-
-      let index = 0;
-      function recursion(deepElement){
-        if(deepElement.element === findElem) return index;
-        else if(deepElement.next === null) return -1;
-        index++;
-        return recursion(deepElement.next);
-      }
-      // loop
-      return recursion(head);
-    };
-
-    /** @param index - element @ index or undefined*/
-    this.elementAt = function(index){
-      if(index > this.size() || this.isEmpty()) {return undefined}; // catch out of range
-
-      let counter = 0;
-      function recursion(deepElement){
-        if(index === counter) return deepElement.element;
-        else if(deepElement.next === null) return undefined;
-        counter++;
-        return recursion(deepElement.next);
-      }
-      // loop
-      return recursion(head);
-    };
-
-    this.addAt = function(index, newElement){
-      if(index >= this.size() || index < 0) {return false} // catch out of range
-
-      length++ // increment length
-
-      // head replace
-      if(index === 0){ 
-        let chain = head;
-        head = new Node(newElement);
-        head.next = chain;
-        return true;
-      }
-
-      let indexPosition = 0;
-      let prevNode = head;
-  
-      // start loop
-      return recursion(head);
-      function recursion(deepNode){
-        if(indexPosition === index){ // set new node to preNode's ref, set newNode's ref to old node (ie deepNode)
-          let chain = deepNode;
-          let newChain = new Node(newElement);
-          prevNode.next = newChain;
-          newChain.next = chain;
-          return true
-        }
-        indexPosition++;
-        prevNode = deepNode;
-        return recursion(deepNode.next)
-      }
-    }
-
-    this.remove = function(element){
-      if(head.element === element){
-        length--
-        return head = head.next;
-      }
-
-      let prevNode = head;
-  
-      function recursion(deepElement){
-        if(deepElement.element === element){
-          length-- 
-          return prevNode.next = deepElement.next;
-        }
-        if(deepElement.next !== null){
-          prevNode = deepElement;
-          recursion(deepElement.next)
-        }
-        return 'Removed'
-      }
-
-      // loop
-      recursion(head.next)
-    };
-
-    this.removeAt = function(index){
-      if(index >= this.size() || index < 0) {return null} // catch out of range
-      if(this.size() === 1) {let r = head.element; head = null; length--; return r} // if only one element, delete and return it
-
-      let indexPosition = 0;
-      let prevNode = head;
-
-      // keep memory of prevNode, recursively dig into chain using ref of the next node
-      return recursion(head);
-      function recursion(deepElement){
-        if(indexPosition === index){
-          length--;
-          prevNode.next = deepElement.next; // replace upper node with the removed node's chain.
-          return deepElement.element;
-        }
-        if(deepElement.next !== null){ 
-          indexPosition++;
-          prevNode = deepElement; // save prev node, continue loop
-          return recursion(deepElement.next)
-        }
-      }
-    };
-  }
-
-  export { LinkedList, LinkedListNode }
