e274a28c019dbd25265a02aa02ac1ac19c63313c
Christian Claudeaux <christian75armand@gmail.com>
2023-04-11 17:57:59 -0400

    avl tree

---
+++ Data-Structures/avl-tree.ts
@@ -0,0 +1,178 @@
+export class AVLTreeNode {
+    value: number;
+    left: AVLTreeNode | null;
+    right: AVLTreeNode | null;
+    height: number;
+  
+    constructor(value: number) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+      this.height = 1;
+    }
+  }
+  
+  export class AVLTree {
+    root: AVLTreeNode | null;
+  
+    constructor() {
+      this.root = null;
+    }
+  
+    private getHeight(node: AVLTreeNode | null): number {
+      if (!node) {
+        return 0;
+      }
+      return node.height;
+    }
+  
+    private getBalanceFactor(node: AVLTreeNode | null): number {
+      if (!node) {
+        return 0;
+      }
+      return this.getHeight(node.left) - this.getHeight(node.right);
+    }
+  
+    private updateHeight(node: AVLTreeNode | null) {
+      if (!node) {
+        return;
+      }
+      node.height = Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
+    }
+  
+    private rotateLeft(node: AVLTreeNode): AVLTreeNode {
+      const newRoot = node.right;
+      const leftSubtree = newRoot!.left;
+  
+      newRoot!.left = node;
+      node.right = leftSubtree;
+  
+      this.updateHeight(node);
+      this.updateHeight(newRoot);
+  
+      return newRoot as AVLTreeNode;
+    }
+  
+    private rotateRight(node: AVLTreeNode): AVLTreeNode {
+      const newRoot = node.left;
+      const rightSubtree = newRoot!.right;
+  
+      newRoot!.right = node;
+      node.left = rightSubtree;
+  
+      this.updateHeight(node);
+      this.updateHeight(newRoot);
+  
+      return newRoot as AVLTreeNode;
+    }
+  
+    private rebalance(node: AVLTreeNode): AVLTreeNode {
+      this.updateHeight(node);
+  
+      const balanceFactor = this.getBalanceFactor(node);
+  
+      if (balanceFactor > 1) {
+        if (this.getBalanceFactor(node.left) < 0) {
+          node.left = this.rotateLeft(node.left!);
+        }
+        return this.rotateRight(node);
+      } else if (balanceFactor < -1) {
+        if (this.getBalanceFactor(node.right) > 0) {
+          node.right = this.rotateRight(node.right!);
+        }
+        return this.rotateLeft(node);
+      }
+  
+      return node;
+    }
+  
+    insert(value: number) {
+      const newNode = new AVLTreeNode(value);
+  
+      if (!this.root) {
+        this.root = newNode;
+        return;
+      }
+  
+      const insertHelper = (node: AVLTreeNode) => {
+        if (value < node.value) {
+          if (!node.left) {
+            node.left = newNode;
+          } else {
+            insertHelper(node.left);
+          }
+        } else {
+          if (!node.right) {
+            node.right = newNode;
+          } else {
+            insertHelper(node.right);
+          }
+        }
+  
+        node = this.rebalance(node);
+      };
+  
+      insertHelper(this.root);
+    }
+  
+    search(value: number): AVLTreeNode | null {
+      let currentNode = this.root;
+  
+      while (currentNode) {
+        if (currentNode.value === value) {
+          return currentNode;
+        } else if (value < currentNode.value) {
+          currentNode = currentNode.left;
+        } else {
+          currentNode = currentNode.right;
+        }
+      }
+  
+      return null;
+    }
+
+    remove(value: number) {
+        this.root = this.removeHelper(this.root, value);
+      }
+    
+      private findMinimum(node: AVLTreeNode): AVLTreeNode {
+        while (node.left) {
+          node = node.left;
+        }
+        return node;
+      }
+    
+      private removeHelper(node: AVLTreeNode | null, value: number): AVLTreeNode | null {
+        if (!node) {
+          return null;
+        }
+    
+        if (value < node.value) {
+          node.left = this.removeHelper(node.left, value);
+        } else if (value > node.value) {
+          node.right = this.removeHelper(node.right, value);
+        } else {
+          // Case 1: No child
+          if (!node.left && !node.right) {
+            node = null;
+          }
+          // Case 2: One child
+          else if (!node.left || !node.right) {
+            node = node.left || node.right;
+          }
+          // Case 3: Two children
+          else {
+            const minRightNode = this.findMinimum(node.right);
+            node.value = minRightNode.value;
+            node.right = this.removeHelper(node.right, minRightNode.value);
+          }
+        }
+    
+        if (!node) {
+          return null;
+        }
+    
+        node = this.rebalance(node);
+        return node;
+      }
+}
---
+++ tests/ds/avl-tree.test.ts
@@ -0,0 +1,105 @@
+import { AVLTree } from '../../Data-Structures/avl-tree';
+
+////////////////////////////////
+// what our tree looks like 
+///////////////////////////////
+//          50
+//        /    \
+//      25      75
+//     /  \    /  \
+//   12   37  60   90
+//  / \   /  \    /  \
+// 10 15 30  40  80  95
+//         / \
+//        55 65
+////////////////////////////////
+
+
+describe("AVLTree", () => {
+  let avlTree: AVLTree;
+  const values = [50, 25, 75, 12, 37, 60, 90, 10, 15, 30, 40, 55, 65, 80, 95];
+
+  beforeEach(() => {
+    avlTree = new AVLTree();
+
+    values.forEach((value) => {
+        avlTree.insert(value);
+      });
+  });
+
+  test("inserts and balances nodes correctly", () => {
+    // Check that the tree has the correct structure and heights
+    expect(avlTree.root?.value).toBe(50);
+    expect(avlTree.root?.height).toBe(4);
+
+    expect(avlTree.root?.left?.value).toBe(25);
+    expect(avlTree.root?.left?.height).toBe(3);
+
+    expect(avlTree.root?.left?.left?.value).toBe(12);
+    expect(avlTree.root?.left?.left?.height).toBe(2);
+
+    expect(avlTree.root?.left?.left?.left?.value).toBe(10);
+    expect(avlTree.root?.left?.left?.left?.height).toBe(1);
+
+    expect(avlTree.root?.left?.left?.right?.value).toBe(15);
+    expect(avlTree.root?.left?.left?.right?.height).toBe(1);
+
+    expect(avlTree.root?.left?.right?.value).toBe(37);
+    expect(avlTree.root?.left?.right?.height).toBe(2);
+
+    expect(avlTree.root?.left?.right?.left?.value).toBe(30);
+    expect(avlTree.root?.left?.right?.left?.height).toBe(1);
+
+    expect(avlTree.root?.left?.right?.right?.value).toBe(40);
+    expect(avlTree.root?.left?.right?.right?.height).toBe(1);
+
+    expect(avlTree.root?.right?.value).toBe(75);
+    expect(avlTree.root?.right?.height).toBe(3);
+
+    expect(avlTree.root?.right?.left?.value).toBe(60);
+    expect(avlTree.root?.right?.left?.height).toBe(2);
+
+    expect(avlTree.root?.right?.left?.left?.value).toBe(55);
+    expect(avlTree.root?.right?.left?.left?.height).toBe(1);
+
+    expect(avlTree.root?.right?.left?.right?.value).toBe(65);
+    expect(avlTree.root?.right?.left?.right?.height).toBe(1);
+
+    expect(avlTree.root?.right?.right?.value).toBe(90);
+    expect(avlTree.root?.right?.right?.height).toBe(2);
+
+    expect(avlTree.root?.right?.right?.left?.value).toBe(80);
+    expect(avlTree.root?.right?.right?.left?.height).toBe(1);
+
+    expect(avlTree.root?.right?.right?.right?.value).toBe(95);
+    expect(avlTree.root?.right?.right?.right?.height).toBe(1);
+  });
+
+
+    test("removes a leaf node correctly", () => {
+      avlTree.remove(40);
+
+      expect(avlTree.root?.right?.right?.value).toBe(90);
+      expect(avlTree.root?.right?.right?.height).toBe(2);
+
+      expect(avlTree.root?.right?.right?.left?.value).toBe(80);
+      expect(avlTree.root?.right?.right?.left?.height).toBe(1);
+
+      expect(avlTree.root?.right?.right?.right?.value).toBe(95);
+      expect(avlTree.root?.right?.right?.right?.height).toBe(1);
+
+      expect(avlTree.root?.right?.value).toBe(75);
+      expect(avlTree.root?.right?.height).toBe(3);
+
+      expect(avlTree.root?.right?.left?.value).toBe(60);
+      expect(avlTree.root?.right?.left?.height).toBe(2);
+
+      expect(avlTree.root?.right?.left?.left?.value).toBe(55);
+      expect(avlTree.root?.right?.left?.left?.height).toBe(1);
+
+      expect(avlTree.root?.right?.left?.right?.value).toBe(65);
+      expect(avlTree.root?.right?.left?.right?.height).toBe(1);
+    });
+  
+})
+
